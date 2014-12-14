4d90cf7a094497d644dd3b3abff6b24bdeb86097
lexiaoyao123456 <281lihaitao@163.com>
2014-12-15 00:11:22 +0800

    coding for apue

--- 16-11.c
+++ 16-11.c
@@ -5,7 +5,7 @@
 
 int connect_retry(int domain, int type, int protocol, const struct sockaddr* addr, socklen_t alen){
   int numsec, fd;
-
+  int cnt = 1;
   for(numsec = 1; numsec <= MAXSLEEP; numsec<<= 1){
     if( ( fd = socket(domain, type, protocol)) < 0)
       return -1;
@@ -17,6 +17,7 @@ int connect_retry(int domain, int type, int protocol, const struct sockaddr* add
 
     if( numsec <= MAXSLEEP/2 )
       sleep(numsec);
+    printf("connect %d\n ", cnt++);
   }
   return -1;
 }
--- 16-16.c
+++ 16-16.c
@@ -35,7 +35,7 @@ int main(int argc,char* argv[])
 
   if( (err = getaddrinfo(argv[1], "ruptime", &hint, &alist)) != 0 )
     err_quit("getaddrinfo error: %s",gai_strerror(err));
-
+  printf("getaddrinfo successed\n");
   for( aip = alist; aip != NULL; aip = aip->ai_next){
     if( (sockfd = connect_retry(aip->ai_family,SOCK_STREAM, 0, aip->ai_addr,aip->ai_addrlen)) <0 )
       err = errno;
